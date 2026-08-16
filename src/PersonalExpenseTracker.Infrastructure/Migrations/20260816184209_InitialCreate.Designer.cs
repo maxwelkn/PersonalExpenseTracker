a5ccae9808a5f9c67b0207c099491401040c7c8b
maxwelkn <saelmaxwel@gmail.com>
2026-08-16 14:46:03 -0400

    chore(project): retarget solution to .NET 9

--- src/PersonalExpenseTracker.Infrastructure/Migrations/20260816184209_InitialCreate.Designer.cs
+++ src/PersonalExpenseTracker.Infrastructure/Migrations/20260816184209_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using PersonalExpenseTracker.Infrastructure.Persistence;
 namespace PersonalExpenseTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260816172214_InitialCreate")]
+    [Migration("20260816184209_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -19,7 +19,7 @@ namespace PersonalExpenseTracker.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.11")
+                .HasAnnotation("ProductVersion", "9.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
--- src/PersonalExpenseTracker.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ src/PersonalExpenseTracker.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace PersonalExpenseTracker.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.11")
+                .HasAnnotation("ProductVersion", "9.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
