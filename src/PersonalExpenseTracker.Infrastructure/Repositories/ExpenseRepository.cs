d38cd18cfedc69a1d15bd19800ba105c24b6ed8c
maxwelkn <saelmaxwel@gmail.com>
2026-08-18 03:12:18 -0400

    feat(expenses): add repository

---
+++ src/PersonalExpenseTracker.Infrastructure/Repositories/ExpenseRepository.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using PersonalExpenseTracker.Application.Interfaces;
+using PersonalExpenseTracker.Domain.Entities;
+using PersonalExpenseTracker.Infrastructure.Persistence;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalExpenseTracker.Infrastructure.Repositories;
+
+public class ExpenseRepository : IExpenseRepository
+{
+    private readonly AppDbContext _context;
+
+    public ExpenseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Expense?> GetByIdAsync(int id)
+    {
+        return await _context.Expenses.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Expense>> GetAllByUserIdAsync(int userId)
+    {
+        return await _context.Expenses
+            .Where(e => e.UserId == userId)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(Expense expense)
+    {
+        _context.Expenses.Add(expense);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Expense expense)
+    {
+        _context.Expenses.Update(expense);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var expense = await _context.Expenses.FindAsync(id);
+        if (expense != null)
+        {
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
